0998be752720692ae88194487f4ad120233aaddf
Sam <samuelgkeller@gmail.com>
2026-03-03 20:03:57 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>JinjaMonogatari CMS</h1>
+      <p>Vite + React + TS is running.</p>
+    </div>
+  );
+}
